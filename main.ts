c1c36b76a15f514d4f024ae46526c4c97efe9653
luckyxicheng <115699265+luckyxicheng@users.noreply.github.com>
2022-10-13 17:08:30 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,19 @@
+function JSJ2 () {
+    basic.showString("197/23=")
+    basic.showNumber(197 / 23)
+}
+function JSJ3 () {
+    basic.showString("697+510=")
+    basic.showNumber(697 + 510)
+}
+function JSJ4 () {
+    basic.showString("733+510=")
+    basic.showNumber(733 + 510)
+}
+function JSJ () {
+    basic.showString("36x24=")
+    basic.showNumber(36 * 24)
+}
+basic.forever(function () {
+    JSJ4()
+})
